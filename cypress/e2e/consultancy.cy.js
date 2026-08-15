d956d95485ea1288709d53a37bdc5690b1d34e34
Adriano Lucas <acaldeira10@gmail.com>
2026-08-15 15:38:04 -0300

    ajustes

--- cypress/e2e/consultancy.cy.js
+++ cypress/e2e/consultancy.cy.js
@@ -1,111 +1,124 @@
-const { channel } = require("process");
+import { personal, company } from "../fixtures/consultancy.json";
 
-describe("Formulário de Consultoria", () => {
+Cypress.Commands.add("fillConsultancyForm", (form) => {
+  cy.get("#name").clear();
 
-beforeEach(() => {
-  cy.login();
-  cy.goTo("Formulários", "Consultoria");
-})
+  cy.get("#name").type(form.name);
+  cy.get("#email").type(form.email);
+  cy.get("#phone").type(form.phone).should("have.value", "(11) 99999-1000");
+  cy.get("#consultancyType").select(form.consultancyType);
 
-  it("Deve solicitar consultoria individual", () => {
-    
-    cy.get("#name").type("Fernando Papito");
-    cy.get("#email").type("fernando.papito@webdojo.com");
+  if (form.personType === 'cpf') {
+    cy.contains('label', 'Pessoa Física')
+      .find('input')
+      .check()
+      .should('be.checked');
 
-    cy.get("#phone")
-      .type("11 99999-1000")
-      .should("have.value", "(11) 99999-1000");
+    cy.contains('label', 'Pessoa Jurídica')
+      .find('input')
+      .should('be.not.checked');
+  }
 
-    cy.get("#consultancyType").select("In Company");
-
-    cy.contains("label", "Pessoa Física")
+  if (form.personType === 'cnpj') {
+    cy.contains('label', 'Pessoa Jurídica')
       .find("input")
       .check()
       .should("be.checked");
 
-    cy.contains("label", "Pessoa Jurídica")
-      .find("input")
-      .should("be.not.checked");
+    cy.contains('label', 'Pessoa Física')
+      .find('input')
+      .should('be.not.checked');
 
-    cy.get("#document")
-      .type("41515055043")
-      .should("have.value", "415.150.550-43");
+    cy.contains('label','CNPJ')
+    .parent()
+    .find('input')
+    .type(form.document)
+  }
 
-    const discorveryChannels = [
-      "Instagram",
-      "LinkedIn",
-      "Udemy",
-      "YouTube",
-      "Indicação de Amigo",
-    ];
 
-    discorveryChannels.forEach((channel) => {
-      cy.contains("label", channel).find("input").check().should("be.checked");
-    });
+  form.discorveryChannels.forEach((channel) => {
+    cy.contains("label", channel).find("input").check().should("be.checked");
+  });
 
-    cy.get('input[type="file"]').selectFile("./cypress/fixtures/document.pdf", {
-      force: true,
-    });
+  cy.get('input[type="file"]').selectFile(form.file, {
+    force: true,
+  });
 
-    cy.get("#details").type("teste campo texto");
+  cy.get("#details").type(form.description);
 
-    const technologies = [
-      "Cypress",
-      "Playwright",
-      "selenium",
-      "Robot Framework",
-      "Cucumber",
-    ];
+  form.tech.forEach((tech) => {
+    cy.get("#technologies").type(tech).type("{enter}");
 
-    technologies.forEach((technologies) => {
-      cy.get("#technologies").type(technologies).type("{enter}");
+    cy.contains("label", "Tecnologias")
+      .parent()
+      .contains("span", tech)
+      .should("be.visible");
+  });
 
-      cy.contains("label", "Tecnologias")
-        .parent()
-        .contains("span", technologies)
-        .should("be.visible");
-    });
-
+  if (form.termsOfUse === true) {
     cy.contains("label", "termos de uso")
       .find("input")
       .check()
       .should("be.checked");
+  }
+});
+
+describe("Formulário de Consultoria", () => {
+  beforeEach(() => {
+    cy.login();
+    cy.goTo("Formulários", "Consultoria");
+    cy.fixture("consultancy").as("consultancyData");
+  });
+
+  it("Deve solicitar consultoria individual", () => {
+    cy.fillConsultancyForm(personal);
 
     cy.contains("button", "Enviar formulário").click();
 
-  
+    cy.get(".modal", { timeout: 70000 })
+      .should("be.visible")
+      .find(".modal-content")
+      .should("be.visible")
+      .should(
+        "have.text",
+        "Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.",
+      );
+  });
 
-    cy.get('.modal', {timeout: 70000})
-    .should('be.visible')
-    .find('.modal-content')
-    .should('be.visible')
-    .should('have.text', 'Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
+  it("Deve solicitar consultoria In Company", () => {
+    cy.fillConsultancyForm(company);
 
+    cy.contains("button", "Enviar formulário").click();
+
+    cy.get(".modal", { timeout: 70000 })
+      .should("be.visible")
+      .find(".modal-content")
+      .should("be.visible")
+      .should(
+        "have.text",
+        "Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.",
+      );
   });
 
   it("Deve verificar os campos obrigatórios", () => {
+    cy.contains("button", "Enviar formulário").click();
 
-    cy.contains("button", "Enviar formulário")
-    .click();
+    cy.contains("label", "Nome Completo")
+      .parent()
+      .find("p")
+      .scrollIntoView()
+      .should("be.visible")
+      .should("have.text", "Campo obrigatório");
 
-    cy.contains('label','Nome Completo')
-    .parent()
-    .find('p')
-    .scrollIntoView()
-    .should('be.visible')
-    .should('have.text','Campo obrigatório');
+    cy.contains("label", "Email")
+      .parent()
+      .find("p")
+      .scrollIntoView()
+      .should("be.visible")
+      .should("have.text", "Campo obrigatório");
 
-    cy.contains('label','Email')
-    .parent()
-    .find('p')
-    .scrollIntoView()
-    .should('be.visible')
-    .should('have.text','Campo obrigatório');
-  
-
-     cy.contains('p', 'Você precisa aceitar os termos de uso')
-    .scrollIntoView()
-    .should('be.visible');
-
+    cy.contains("p", "Você precisa aceitar os termos de uso")
+      .scrollIntoView()
+      .should("be.visible");
   });
 });
